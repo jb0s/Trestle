01ba4eca3ec4a1960768502ed283007c4b63a075
Jake <me@irma.rip>
2021-05-29 14:01:43 +0200

    Added ItemType and IBlockItem

---
+++ Trestle/Enums/ItemType.cs
@@ -0,0 +1,35 @@
+﻿namespace Trestle.Enums
+{
+    public enum ItemType
+    {
+        /// <summary>
+        /// The default item type.
+        /// </summary>
+        All,
+        
+        /// <summary>
+        /// Melee weapons, such as swords.
+        /// </summary>
+        Melee,
+        
+        /// <summary>
+        /// Tools that act as pickaxes.
+        /// </summary>
+        Pickaxe,
+        
+        /// <summary>
+        /// Tools that act as axes.
+        /// </summary>
+        Axe,
+        
+        /// <summary>
+        /// Tools that act as shovels.
+        /// </summary>
+        Shovel,
+        
+        /// <summary>
+        /// Tools that act as hoes.
+        /// </summary>
+        Hoe
+    }
+}
--- Trestle/Levels/Items/Blocks/IBlockItem.cs
+++ Trestle/Levels/Items/Blocks/IBlockItem.cs
@@ -1,7 +1,18 @@
-﻿namespace Trestle.Levels.Items.Blocks
+﻿using Trestle.Enums;
+
+namespace Trestle.Levels.Items.Blocks
 {
-    public interface IBlockItem
+    public interface IBlockItem : IItem
     {
-        
+        /// <summary>
+        /// The type of item required to drop this block's loot.
+        /// </summary>
+        public ItemType RequiredTool => ItemType.All;
+
+        /// <summary>
+        /// The items that this block drops when broken by the right tool.
+        /// </summary>
+        /// <returns></returns>
+        public Material[] Loot => new Material[] { Material.Stone };
     }
 }
